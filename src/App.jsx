5b765cbba732a00dbab8ea79da1247760eae676e
desmond009 <ragnarviju2004@gmail.com>
2025-10-05 14:47:46 +0530

    Query Client and Connector

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,50 @@
-import { WagmiProvider } from 'wagmi'
+import { useAccount, useConnect, useConnectors, useDisconnect, WagmiProvider } from 'wagmi'
 import { config } from './config/config.js'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './App.css'
+
+const queryClient = new QueryClient()
 
 function App() {
 
   return (
     <WagmiProvider config={config}>
-      HI There
+      <QueryClientProvider client={queryClient}>
+        <ConnectWallet />
+      </QueryClientProvider>
     </WagmiProvider>
+  )
+}
+
+function ConnectWallet() {
+
+  // useAccount hook ==> is this user connected or not
+  const { address } = useAccount()
+  // useConnectors hook ==> Will return me a list of connectors
+  const connectors = useConnectors()
+  // useDisconnect hook ==> Will disconnect the user
+  const { disconnect } = useDisconnect()
+
+  const { connect, connector } = useConnect()  
+
+  // If the user already connected to a wallet
+  if (address) {
+    return (
+      <div>
+        <p>You are Connected to {address}</p>
+        <button onClick = {() => disconnect()}>Disconnect</button>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      {connectors.map(connector_c => 
+        <button key={connector_c.id} onClick={() => connect({ connector: connector_c})}>
+          Connect via {connector_c.name}
+        </button>
+      )}
+    </div>
   )
 }
 
--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,10 @@
 import { http , createConfig } from 'wagmi'
 import { mainnet } from 'wagmi/chains'
+import { injected, metaMask } from 'wagmi/connectors'
 
 export const config = createConfig({
     chains: [mainnet],
+    connectors: [ injected() ],
     transports : {
         [mainnet.id]: http("https://eth-mainnet.g.alchemy.com/v2/6mFCPorjtiIGk-WlzevtyUEXl0xHqseb")
     },
